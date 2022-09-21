0ed2d3651a1d59268776f6c4b25b35a73f8306bf
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-09-20 23:13:41 -0600

    Initial Commit

---
+++ InventoryList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InventoryList
+//
+//  Created by Byron Mejia on 9/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
